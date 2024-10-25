2ed91f452e4a795bc08b3725cdc130da2b33a997
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2024-10-25 21:56:57 +0530

    add template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+// submitted by Mohit Verma "mohitvdx"
+
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); // fast IO
+
+    // int t;
+    // cin>>t;
+    // while(t--){
+
+    // }
+}
