9002968377720ba95417a9b5e593c4cb67dbf161
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2024-11-13 14:40:15 +0530

    add power in logn TC

--- cpp_basics/factorial.cpp
+++ cpp_basics/factorial.cpp
@@ -1,18 +1,20 @@
-#include <bits/stdc++.h>
+#include <stdc++.h>
 using namespace std;
 
-int main(){
-int t;
-cin>>t;
-while(t--){
-int a;
-cin>>a;
-long long int factorial=1;
-for (int i=a;i>0;i--){
-factorial = factorial*i;
-}
-cout<<factorial<<endl;
-}
-return 0;
-
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a;
+        long long int factorial = 1;
+        for (int i = a; i > 0; i--)
+        {
+            factorial = factorial * i;
+        }
+        cout << factorial << endl;
+    }
+    return 0;
 }
--- level 1/power.cpp
+++ level 1/power.cpp
@@ -1,3 +1,29 @@
 //
 // Created by Mohit verma on 13/11/24.
 //
+
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int power(int a, int b){
+    if(b==0){
+        return 1 ;
+    }
+
+    int half_power = power(a,b/2);
+    if(b%2==0){
+        return half_power*half_power;
+    }else{
+        return half_power*half_power*a;
+    }
+}
+
+signed main(){
+
+    cout<<power(3,7);
+
+}
+
+
+//this method of finding the power takes a time complexity of O(log n)
